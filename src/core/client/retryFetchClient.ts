265f9561e119caa12cffb1d602cd1bfa4dda49aa
Fabian Jocks <dev@jocks.io>
2021-12-23 12:15:54 +0100

    Added collector for error messages to retry client

--- src/core/client/retryFetchClient.ts
+++ src/core/client/retryFetchClient.ts
@@ -13,16 +13,18 @@ export class RetryFetchClient extends FetchClient {
 
   public async fetch<R>(request: () => Promise<R>): Promise<R> {
     let count: number = 0;
+    let errors: any[] = [];
 
     while (count++ < this.options.limit) {
       try {
         return Promise.resolve(await request());
       } catch (err: any) {
         this.logger.info(`Rejected! Waiting ${this.options.delay}s [${count}/${this.options.limit}]`);
+        errors.push(err);
         await new Promise((resolve) => setTimeout(resolve, this.options.delay * 1000));
       }
     }
 
-    return Promise.reject(`Could not execute request after ${this.options.limit} retries`);
+    return Promise.reject(`Could not execute request after ${this.options.limit} retries! Errors: \n${errors.join('\n\t')}`);
   }
 }
